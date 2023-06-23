b7742750f228ad71af4b9aec684d97bab5e029a3
luckyfullstack2023 <luckyfullstack2023@gmail.com>
2023-06-24 02:30:36 +0530

    sir L-54 from batch-2 assignment done

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+let news = {
+    h1 :'Leading',
+    h2 :"American",
+    h3 :`newspapers`,
+    h4 :'highlighted',
+    h5 :"Prime",
+    h6 :`Minister`,
+    h7 :'Narendra',
+    h8 :"Modi’s",
+    h9 :`first`,
+    h10 :'state',
+    h11 :"visit",
+    h12:`to`,
+    h13:'the',
+    h14 :"US,",
+    h15 :`with`,
+    h16 :'prominent',
+    h17 :"coverage",
+    h18 :`including`,
+    h19 :'reports',
+    h20 :"on",
+    h21 :`technology`,
+    h22 :'and',
+    h23 :"defence",
+    h24 :`agreements`,
+    h25 :'between',
+    h26 :"the",
+    h27 :`two`,
+    h28:'countries.',
+   
+}
+// expression
+let word1 = "h3"
+let word2 = "h6"
+let word3 = "h9"
+let word4 = "h12"
+let word5 = "h15"
+let word6 = "h18"
+let word7 = "h21"
+let word8 = "h24"
+let word9 = "h27"
+console.log(news.h1+' '+news["h2"]+" "+news[word1]+` `+news.h4+' '+news["h5"]+" "+news[word2]+` `+news.h7+' '+news["h8"]+" "+news[word3]+` `+news.h10+' '+news["h11"]+" "+news[word4]+` `+news.h13+' '+news["h14"]+" "+news[word5]+` `+news.h16+' '+news["h17"]+" "+news[word6]+` `+news.h19+' '+news["h20"]+" "+news[word7]+` `+news.h22+' '+news["h23"]+" "+news[word8]+` `+news.h25+' '+news["h26"]+" "+news[word9]+` `+news.h28);
